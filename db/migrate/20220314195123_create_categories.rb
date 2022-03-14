2cc7ba34870e774f600d049fcc93160828512f20
Hawell15 <roma.ciobanu@gmail.com>
2022-03-14 21:51:37 +0200

    category scaffold

---
+++ db/migrate/20220314195123_create_categories.rb
@@ -0,0 +1,11 @@
+class CreateCategories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :categories do |t|
+      t.string :category_name
+      t.string :full_name
+      t.float :points
+
+      t.timestamps
+    end
+  end
+end
